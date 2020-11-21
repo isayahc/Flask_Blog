7073c7b4a52c62900be41f5993501f9c51e0d4bf
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-20 19:56:40 -0500

    changed config

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -21,7 +21,7 @@ oauth = OAuth()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.ProductionConfig')
 
     #import db
     from .model import db
