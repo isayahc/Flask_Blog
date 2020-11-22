8dde1d01586ab8b2ee447af0baefa190844d5ed3
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-22 11:07:16 -0500

    adding mark down library

--- flaskr/blueprints/post/forms.py
+++ flaskr/blueprints/post/forms.py
@@ -3,7 +3,10 @@ from wtforms import StringField, TextAreaField
 from wtforms.fields.simple import SubmitField
 from wtforms.validators import DataRequired
 
+from flaskr import pagedown
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
